91165415fe3ddbcdea2cf512ec667f9036c0ca4e
KHITER Med Achraf <khiter.med7@gmail.com>
2021-04-11 14:14:31 +0100

    add : about file

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def home():
     return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug= True)
