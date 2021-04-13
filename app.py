8026d1051a1b0cbde9d0cf3f3294aa80fba3fb3a
KHITER Med Achraf <khiter.med7@gmail.com>
2021-04-13 18:15:10 +0100

    add : link to articles

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
-
+from data import Articles
 app = Flask(__name__)
 
-
+Articles = Articles()
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -10,6 +10,14 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('/articles.html', articles = Articles)
+
+@app.route('/article/<string:id>/')
+def article(id):
+    return render_template('/article.html', id = id)
 
 if __name__ == '__main__':
     app.run(debug= True)
