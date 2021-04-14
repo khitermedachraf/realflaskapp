39070091142baa7a91be7a93247bc4ed9f7b51bc
KHITER Med Achraf <khiter.med7@gmail.com>
2021-04-14 01:15:46 +0100

    add: register data to DB & display msg

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template
-from data import Articles
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+#from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
+
 app = Flask(__name__)
 
-Articles = Articles()
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '123456'
+app.config['MYSQL_DB'] = 'myflaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# init MYSQL
+mysql = MySQL(app)
+
+# Articles = Articles()
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -11,13 +25,43 @@ def home():
 def about():
     return render_template('about.html')
 
+# Articles
 @app.route('/articles')
 def articles():
-    return render_template('/articles.html', articles = Articles)
+    # Create cursor
+    cur = mysql.connection.cursor()
+
+    # Get articles
+    result = cur.execute("SELECT * FROM articles")
+
+    articles = cur.fetchall()
+
+    if result > 0:
+        return render_template('articles.html', articles=articles)
+    else:
+        msg = 'No Articles Found'
+        return render_template('articles.html', msg=msg)
+    # Close connection
+    cur.close()
+
 
 @app.route('/article/<string:id>/')
 def article(id):
     return render_template('/article.html', id = id)
 
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+
+
+# Register Form Class
 if __name__ == '__main__':
     app.run(debug= True)
